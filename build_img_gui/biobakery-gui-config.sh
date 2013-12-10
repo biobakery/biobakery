910e238f48a4652d4a78333f428db170f4606f63
Eric Franzosa <franzosa@hsph.harvard.edu>
2013-12-10 14:26:31 -0500

    new approach where gui hg pulls

--- build_img_gui/biobakery-gui-config.sh
+++ build_img_gui/biobakery-gui-config.sh
@@ -6,62 +6,54 @@
 # constants
 # ---------------------------------------------------------------
 
-UNAME=$USER
-FOLDER_SETUP="$HOME/biobakery_setup"
+#URL_DROPBOX="https://dl.dropboxusercontent.com/u/1391730/biobakery"
+URL_BITBUCKET="https://bitbucket.org/timothyltickle/biobakery"
+FOLDER_SETUP="$HOME/biobakery/build_gui_img"
 FOLDER_DESKTOP="$HOME/Desktop"
-FOLDER_BUILD="build_img_gui"
-URL_BITBUCKET="https://bitbucket.org/timothyltickle/biobakery/src/$FOLDER_BUILD"
-FILE_WALLPAPER="$FOLDER_SETUP/$FOLDER_BUILD/bioBakeryWallpaper.png"
-FILE_BASHRC="$FOLDER_SETUP/$FOLDER_BUILD/bashrc"
-
-# ---------------------------------------------------------------
-# set up new directories
-# ---------------------------------------------------------------
-
-mkdir $SETUP
-chmod 777 $SETUP
-cd $SETUP
+FILE_WALLPAPER="bioBakeryWallpaper.png"
+FILE_BASHRC="bashrc"
+FILE_README="README.html"
 
 # ---------------------------------------------------------------
 # work with standard ubuntu repos
 # ---------------------------------------------------------------
 
 # remove unnecessary big installs
-sudo apt-get autoremove libreoffice thunderbird
+sudo apt-get autoremove libreoffice* thunderbird*
 
 # add virtualbox guest additions
 # **** see: http://askubuntu.com/questions/22743/how-do-i-install-guest-additions-in-virtualbox ****
-apt-get install build-essential linux-headers-`uname -r` dkms
-apt-get install apt-get install virtualbox-ose-guest-x11
+sudo apt-get install build-essential linux-headers-`uname -r` dkms
+sudo apt-get install virtualbox-ose-guest-x11
 
 # add dev utilities
-apt-get install mercurial git gdebi-core 
+sudo apt-get install mercurial git gdebi-core python-pip
 
 # add general linux utilities
-apt-get install emacs24 gnome-open trash-cli
+sudo apt-get install trash-cli libgnome2-bin emacs24
 
 # ---------------------------------------------------------------
 # gui configuration
 # ---------------------------------------------------------------
 
-# clone down files
+# set up environment
+# mkdir $FOLDER_SETUP
+# chmod 777 $FOLDER_SETUP
+
+# hg clone bitbucket repo
+cd $HOME
 hg clone $URL_BITBUCKET
 
-# get/place the readme file
-# wget -P $DESKTOP https://dl.dropboxusercontent.com/u/1391730/biobakery.png
-
 # get/set the wallpaper centered with a black background
-# wget -P $SETUP https://dl.dropboxusercontent.com/u/1391730/biobakery.png
-sudo -iu $UNAME sh -c 'gsettings set org.gnome.desktop.background picture-uri "file://$FILE_WALLPAPER"'
-sudo -iu $UNAME sh -c 'gsettings set org.gnome.desktop.background picture-options "centered"'
-sudo -iu $UNAME sh -c 'gsettings set org.gnome.desktop.background primary-color "#000000"'
+gsettings set org.gnome.desktop.background picture-uri "file://$FOLDER_SETUP/$FILE_WALLPAPER"
+gsettings set org.gnome.desktop.background picture-options "centered"
+gsettings set org.gnome.desktop.background primary-color "#000000"
 
 # simplify the ubuntu launcher
-LAUNCHER="['application://nautilus-home.desktop', 'application://firefox.desktop', 'application://gnome-control-center.desktop', 'unity://running-apps', 'application://gnome-terminal.desktop', 'unity://expo-icon', 'unity://devices']"
-sudo -iu $UNAME sh -c "gsettings set com.canonical.Unity.Launcher favorites $LAUNCHER"
+gsettings set com.canonical.Unity.Launcher favorites "['application://nautilus-home.desktop', 'application://firefox.desktop', 'application://gnome-control-center.desktop', 'unity://running-apps', 'application://gnome-terminal.desktop', 'unity://expo-icon', 'unity://devices']"
 
 # replace the buildin bashrc file
-mv $FILE_BASHRC $HOME/.bashrc
+cp $FOLDER_SETUP/$FILE_BASHRC $HOME/.bashrc
 source $HOME/.bashrc
 
 # ---------------------------------------------------------------
@@ -69,9 +61,16 @@ source $HOME/.bashrc
 # ---------------------------------------------------------------
 
 # add custom repo to the sources lists
-# echo "deb http://biobakery.bitbucket.org/deb-packages ./" >> /etc/apt-sources.list
+# **** note: special use of tee command to append to sources.list, since >> doesn't work in sudo context ****
+echo "deb http://biobakery.bitbucket.org/deb-packages ./" | sudo tee -a /etc/apt/sources.list
+sudo apt-get update
 
-#
+# install things from the repo
+sudo apt-get install qiimetomaaslin
+# sudo apt-get install breadcrumbs 
+# sudo apt-get install graphlan
+# sudo apt-get install humann
+# sudo apt-get install micropita
 
 # ---------------------------------------------------------------
 # gdebi custom linked debs
