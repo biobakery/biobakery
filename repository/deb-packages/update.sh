bad819de2f4555c746fe884c257b39ceec9bc78d
Russell Schwager <2321439+rschwager@users.noreply.github.com>
2014-02-10 11:45:36 -0500

    moving file repository scripts over

---
+++ repository/deb-packages/update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+chmod 664 *.deb *.gz
+cd ..
+tar cz deb-packages | ssh huttenhower.rc.fas.harvard.edu "umask 002; cd /usr/local/www/html/biobakery-shop/; rm deb-packages/*.deb; tar -xvzf -" 2>&1 | egrep -v 'utime|failure'
+cd -
