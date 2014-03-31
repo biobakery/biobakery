d2b63ba31afdb4b589e0dceb6d29ebc5a91c905a
rschwager-hsph <rschwager-hsph@localhost>
2014-03-31 08:41:08 -0400

    fix: remove gz files on import, too

--- repository/deb-packages/update.sh
+++ repository/deb-packages/update.sh
@@ -2,5 +2,5 @@
 
 chmod 664 *.deb *.gz
 cd ..
-tar cz deb-packages | ssh huttenhower.rc.fas.harvard.edu "umask 002; cd /usr/local/www/html/biobakery-shop/; rm deb-packages/*.deb; tar -xvzf -" 2>&1 | egrep -v 'utime|failure'
+tar cz deb-packages | ssh huttenhower.rc.fas.harvard.edu "umask 002; cd /usr/local/www/html/biobakery-shop/; rm deb-packages/*.{deb,gz}; tar -xvzf -" 2>&1 | egrep -v 'utime|failure'
 cd -
