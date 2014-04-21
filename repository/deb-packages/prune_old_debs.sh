d1a3c1c7a70336d357f5dc95f9ae1aba201317fc
Russell Schwager <2321439+rschwager@users.noreply.github.com>
2014-04-21 12:24:23 -0400

    update prune script to accomodate new versioning scheme

--- repository/deb-packages/prune_old_debs.sh
+++ repository/deb-packages/prune_old_debs.sh
@@ -5,11 +5,8 @@ names_list=$(ls *.deb | sed -e 's/_.*.deb//' | sort | uniq)
 echo "${names_list}" | while read name; do
 
     ls ${name}*.deb \
-	| sed -e 's/.*_\([0-9][0-9]*\)_.*/\1/' \
-	| python -c 'import datetime, sys; map( lambda val: sys.stdout.write(str(datetime.datetime.strptime(val.strip(), "%d%m%y").strftime("%s"))+"\n"), sys.stdin)' \
 	| sort -rn \
 	| sed -ne '2,$p' \
-	| xargs -I%n date -d "@%n" "+${name}_%d%m%y_all.deb" \
-	| xargs rm -vf
+	| xargs -r rm -vf 
 
 done
