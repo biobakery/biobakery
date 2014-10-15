fa850fb6bb2871cdbee75e044ece110a474a13c0
rschwager-hsph <rschwager-hsph@localhost>
2014-10-15 12:36:21 -0400

    feature: optionally supply user names for uploading and syncing

--- repository/deb-packages/update.sh
+++ repository/deb-packages/update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+user="$1"
+test -z "${user}" && user=$(whoami)
 
 chmod 664 *.deb *.gz
 cd ..
-tar cz deb-packages | ssh huttenhower.rc.fas.harvard.edu "umask 002; cd /usr/local/www/html/biobakery-shop/; rm deb-packages/*.{deb,gz}; tar -xvzf -" 2>&1 | egrep -v 'utime|failure'
+tar cz deb-packages | ssh "${user}"@huttenhower.rc.fas.harvard.edu "umask 002; cd /usr/local/www/html/biobakery-shop/; rm deb-packages/*.{deb,gz}; tar -xvzf -" 2>&1 | egrep -v 'utime|failure'
 cd -
