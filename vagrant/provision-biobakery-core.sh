173d6717170f1c1e9c018482d7866fe0d2a80c71
Lauren McIver <lauren.j.mciver@gmail.com>
2016-05-19 11:49:13 -0600

    Install openjdk, biobakery tool dependency

--- vagrant/provision-biobakery-core.sh
+++ vagrant/provision-biobakery-core.sh
@@ -42,9 +42,8 @@ apt-get install -y ruby-full R-base
 # install dependencies for numpy and matplotlib
 apt-get install -y python2.7-dev pkg-config
 
-# install dependencies for java installer
-apt-get install -y python-software-properties
-add-apt-repository -y ppa:webupd8team/java
+# install java openjdk for biobakery tools
+apt-get install -y openjdk-8-jre
 
 # install homebrew
 git clone https://github.com/Linuxbrew/linuxbrew.git /home/vagrant/.linuxbrew
