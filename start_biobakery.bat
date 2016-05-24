eaf7a70ea5b5e3b889d67141d5d1ffeecdc55369
Lauren McIver <lauren.j.mciver@gmail.com>
2016-05-24 13:29:46 -0600

    Update windows script comments

--- start_biobakery.bat
+++ start_biobakery.bat
@@ -1,18 +1,18 @@
 @echo off
 
-echo check for vagrant (if installed print version)
+REM check for vagrant (if installed print version)
 vagrant -v
 
-echo check for virtualbox (if install print version)
+REM check for virtualbox (if install print version)
 vboxmanage -v
 
-echo create a biobakery folder in the home directory if it does not already exist
+REM create a biobakery folder in the home directory if it does not already exist
 if not exist %HOMEPATH%\biobakery mkdir %HOMEPATH%\biobakery
 cd %HOMEPATH%\biobakery
 
-echo create biobakery vagrantfile, if it does not exist
+REM create biobakery vagrantfile, if it does not exist
 if not exist %HOMEPATH%\biobakery\Vagrantfile vagrant init biobakery/biobakery
 
-echo start biobakery (and also install the first time command is run)
+REM start biobakery (and also install the first time command is run)
 vagrant up --provider virtualbox
 
