8b9f2d9bcd92fb56e9593771e805da7785f044cd
Lauren McIver <Lauren McIver@localhost>
2016-06-27 16:26:10 -0600

    Remove legacy deb package import, prune, sync, and update scripts

--- repository/deb-packages/sync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-user="$1"
-test -z "${user}" && user=$(whoami)
-
-rsync -avz "${user}"@huttenhower.rc.fas.harvard.edu:/usr/local/www/html/biobakery-shop/deb-packages/ ./
--- repository/deb-packages/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-user="$1"
-test -z "${user}" && user=$(whoami)
-
-chmod 664 *.deb *.gz
-cd ..
-tar cz deb-packages | ssh "${user}"@huttenhower.rc.fas.harvard.edu "umask 002; cd /usr/local/www/html/biobakery-shop/; rm deb-packages/*.{deb,gz}; tar -xvzf -" 2>&1 | egrep -v 'utime|failure'
-cd -
