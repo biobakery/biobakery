600a6f9b79e2448bc0d585a0d78729899a0847f1
Lauren McIver <lauren.j.mciver@gmail.com>
2016-06-27 16:26:55 -0600

    Remove legacy vagrant sync and update scripts

--- repository/vagrant/sync.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rsync -avz huttenhower.rc.fas.harvard.edu:/usr/local/www/html/biobakery-shop/vagrant/ ./
--- repository/vagrant/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-find . -type f | xargs chmod 664
-find . -type d | xargs chmod 775
-cd ..
-tar cz vagrant | ssh huttenhower.rc.fas.harvard.edu "umask 002; cd /usr/local/www/html/biobakery-shop/; rm -rf vagrant; tar -xvzf -" 2>&1 | egrep -v 'utime|failure'
-cd -
