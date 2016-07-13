4a6f94e52b2eb1d937ad8a243846221ef32db94b
Lauren McIver <Lauren McIver@localhost>
2016-07-13 11:26:59 -0600

    Update graphlan demo in biobakery_demos to match metaphlan2 tutorial

--- demos/biobakery_demos/demos/graphlan.bash
+++ demos/biobakery_demos/demos/graphlan.bash
@@ -1,9 +1,11 @@
 # get the version information (demo developed with graphlan v0.9.7)
 graphlan.py --version
 
+# generate the tree structure and annotation file
+export2graphlan.py --skip_rows 1,2 -i $INPUT_FOLDER/merged_abundance_table.txt --tree $OUTPUT_FOLDER/merged_abundance.tree.txt --annotation $OUTPUT_FOLDER/merged_abundance.annot.txt --most_abundant 100 --abundance_threshold 1 --least_biomarkers 10 --annotations 5,6 --external_annotations 7 --min_clade_size 1
+
 # run graphlan annotate on the demo input files
-graphlan_annotate.py $INPUT_FOLDER/hmptree.xml $OUTPUT_FOLDER/hmptree.annot.xml --annot $INPUT_FOLDER/annot.txt
+graphlan_annotate.py --annot $OUTPUT_FOLDER/merged_abundance.annot.txt $OUTPUT_FOLDER/merged_abundance.tree.txt $OUTPUT_FOLDER/merged_abundance.xml
 
 # run graphlan
-graphlan.py $OUTPUT_FOLDER/hmptree.annot.xml $OUTPUT_FOLDER/hmptree.png --dpi 150 --size 14
-
+graphlan.py --dpi 300 $OUTPUT_FOLDER/merged_abundance.xml $OUTPUT_FOLDER/merged_abundance.png --external_legends
