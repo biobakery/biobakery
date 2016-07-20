15dd2d66883bff4deaf67fa488ea1304733bb678
Lauren McIver <lauren.j.mciver@gmail.com>
2016-07-20 14:44:33 -0600

    Update halla demo script and files to latest version

--- demos/biobakery_demos/demos/halla.bash
+++ demos/biobakery_demos/demos/halla.bash
@@ -1,6 +1,8 @@
-# get the version information (demo developed with halla v0.6.1)
+# get the version information (demo developed with halla v0.6.7)
 halla --version
 
 # run halla on the demo input file
 halla -X $INPUT_FOLDER/X_16_100.txt -Y $INPUT_FOLDER/Y_16_100.txt --output $OUTPUT_FOLDER --nproc $THREADS
 
+# run visualization
+hallagram $OUTPUT_FOLDER/similarity_table.txt $OUTPUT_FOLDER/hypotheses_tree.txt $OUTPUT_FOLDER/associations.txt --outfile $OUTPUT_FOLDER/hallagram.pdf
