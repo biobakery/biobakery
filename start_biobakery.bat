f4d4e38245d16e7a40421177ce3b2c3ea8df9e60
Lauren McIver <Lauren McIver@localhost>
2016-07-22 10:21:24 -0600

    Add quotes to windows startup script for paths with spaces (fix from Curtis)

--- start_biobakery.bat
+++ start_biobakery.bat
@@ -4,16 +4,16 @@ REM check for vagrant (if installed print version else print error then wait 30 
 vagrant -v || echo ERROR: Please install vagrant && timeout 30>nul && exit /b
 
 REM check for virtualbox (if installed print version else print error then wait 30 seconds and close window)
-%ProgramFiles%\Oracle\VirtualBox\VBoxManage.exe -v || echo ERROR: Please install virtualbox && timeout 30>nul && exit /b
+"%ProgramFiles%\Oracle\VirtualBox\VBoxManage.exe" -v || echo ERROR: Please install virtualbox && timeout 30>nul && exit /b
 
 REM create a biobakery folder in the home directory if it does not already exist
-if not exist %HOMEPATH%\biobakery mkdir %HOMEPATH%\biobakery
-cd %HOMEPATH%\biobakery
+if not exist "%HOMEPATH%\biobakery" mkdir "%HOMEPATH%\biobakery"
+cd "%HOMEPATH%\biobakery"
 
 echo Thanks for downloading the bioBakery! During the first execution, we'll download a fairly large virtual image with all of the software environment pre-installed. This may take a while depending on your Internet connection, but it only happens once. After that, simply run the bioBakery using this launcher. Thanks for using our software!
 
 REM create biobakery vagrantfile, if it does not exist
-if not exist %HOMEPATH%\biobakery\Vagrantfile vagrant init biobakery/biobakery
+if not exist "%HOMEPATH%\biobakery\Vagrantfile" vagrant init biobakery/biobakery
 
 REM start biobakery (and also install the first time command is run)
 vagrant up --provider virtualbox
