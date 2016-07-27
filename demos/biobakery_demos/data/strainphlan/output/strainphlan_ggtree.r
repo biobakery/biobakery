df8d17d01b5d6c962c824baf0d392aa865ec6d42
Lauren McIver <lauren.j.mciver@gmail.com>
2016-07-27 16:53:50 -0600

    Add galeb's two strainphlan r tutorial scripts

---
+++ demos/biobakery_demos/data/strainphlan/output/strainphlan_ggtree.r
@@ -0,0 +1,41 @@
+# Generate dendrograms from StrainPhlAn outputs
+# Execute script from within working directory that contains these three input files:
+
+# RAxML_bestTree.s__Eubacterium_siraeum.tree
+# metadata.txt
+# s__Eubacterium_siraeum.fasta
+
+# load library ggtree
+library(ggplot2)
+library(ggtree)
+
+
+# read newick tree
+e.sir.tre <- read.tree( 'RAxML_bestTree.s__Eubacterium_siraeum.tree' )
+# create ggtree object
+e.sir.gg <- ggtree( e.sir.tre )
+# read in metadata file
+e.sir.meta <- read.delim( 'metadata.txt', header = T, sep = "\t" )
+# add metadata to dendrogram plot
+e.sir.gg <- e.sir.gg %<+% e.sir.meta
+# plot with color terminal edges and sample names with SubjectID and add tiplabels
+# strainphlan_tree_1.pdf
+e.sir.gg +  
+  #geom_tiplab(size = 1.75, aes( color = SubjectID ) ) + 
+  geom_tippoint( size = 3, aes( color = SubjectID ) ) + 
+  aes( branch.length = 'length' ) +
+  theme_tree2() + theme(legend.position="right")
+
+# visualize tree with multiple sequence alignment (MSA)
+
+# load RColorBrewer library
+library(RColorBrewer)
+
+# path to alignment file
+e.sir.fasta <- ("s__Eubacterium_siraeum.fasta")
+# plot tree with slice of MSA
+# strainphlan_tree_2.pdf
+msaplot( e.sir.gg + geom_tippoint( size = 3, aes( color = SubjectID ) ), 
+         e.sir.fasta, window = c( 490,540 ), 
+         color = brewer.pal(4, "Accent") ) + 
+  theme( legend.position = 'right' )
