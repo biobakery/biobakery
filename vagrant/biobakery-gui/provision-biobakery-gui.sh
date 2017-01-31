5e35bc2881ddaec3c04042126f02f190e2d5dff0
Lauren McIver <lauren.j.mciver@gmail.com>
2017-01-31 14:16:47 -0700

    Create the desktop folder before adding welcome file

--- vagrant/biobakery-gui/provision-biobakery-gui.sh
+++ vagrant/biobakery-gui/provision-biobakery-gui.sh
@@ -99,8 +99,9 @@ sudo -iu vagrant bash -c 'DISPLAY=:0 gsettings set com.canonical.Unity.Launcher 
 # place custom files
 # ---------------------------------------------------------------
 
-# copy the readme to the desktop
-cp $FOLDER_SETUP/$FILE_WELCOME $FOLDER_DESKTOP/$FILE_WELCOME
+# copy the readme to the desktop (first creating the folder)
+sudo -u vagrant mkdir -p $FOLDER_DESKTOP
+sudo -u vagrant cp $FOLDER_SETUP/$FILE_WELCOME $FOLDER_DESKTOP/$FILE_WELCOME
 # link /vagrant to the desktop
 ln -s $FOLDER_SETUP $FOLDER_DESKTOP
 # change terminal settings
