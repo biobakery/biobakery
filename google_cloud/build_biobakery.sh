e8f970e56c78950d4b3b1fe616c07223e792d2d9
ljmciver <lauren.j.mciver@gmail.com>
2019-09-12 09:42:27 -0600

    Use export instead of path to add conda install

--- google_cloud/build_biobakery.sh
+++ google_cloud/build_biobakery.sh
@@ -29,7 +29,7 @@ sudo apt-get install -y texlive pandoc
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda/
 echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
+export PATH="$HOME/miniconda/bin:$PATH"
 conda init bash
 . $HOME/miniconda/etc/profile.d/conda.sh
 
