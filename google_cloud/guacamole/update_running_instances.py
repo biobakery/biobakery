36ede624457d4e086a58ddc621bcbfd1c1ed704e
ljmciver <lauren.j.mciver@gmail.com>
2021-05-26 15:37:06 -0600

    Update script to update instances to handle host key checking by default and also to allow for errors on a single instance but still continue

--- google_cloud/guacamole/update_running_instances.py
+++ google_cloud/guacamole/update_running_instances.py
@@ -1,7 +1,9 @@
 # Run this script to update the running instances
-# $ python update_running_instances.py "cd /tmp && wget file && ls" biobakery.key "10.0.0.0"
+  
+# $ python update_running_instances.py "cd /tmp && wget file && ls" biobakery.key password "10.0.0.0"
+
 # The first arg is the command to execute, the second is a file with the private ssh key
-# the third is the subset of IPs to run on (empty if no subset filter to be applied).
+# the third is the password for the key , and the fourth (optional)  is a subset of IPs to run on (empty if no subset filter to be applied).
 
 import sys
 import subprocess
@@ -26,6 +28,10 @@ if len(biobakery_instances)==0:
 for instance in biobakery_instances:
     if not subset_filter or (subset_filter and instance[2] in subset_filter):
         print("# Update Image: Google Cloud Instance NAME: {0} IP: {1}".format(instance[0], instance[2]))
-        command="sshpass -P passphrase -p {0} ssh {1} -i {2} '{3}'".format(password, instance[2], key_file, user_command)
+        command="sshpass -P passphrase -p {0} ssh -o StrictHostKeyChecking=no {1} -i {2} '{3}'".format(password, instance[2], key_file, user_command)
         print(command)
-        print(subprocess.check_output(command,shell=True))
+        try:
+            print(subprocess.check_output(command,shell=True))
+        except subprocess.CalledProcessError as e:
+            print("Error running command on instance : "+instance[2])
+            print(e)
