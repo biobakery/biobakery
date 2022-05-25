5f69c2ab43e01ad6948832cdd3126b3d53df90d4
ljmciver <lauren.j.mciver@google.com>
2022-05-25 10:13:52 -0600

    Update graphlan to latest demo commands

--- test_suite/biobakery_tests/demos/graphlan.bash
+++ test_suite/biobakery_tests/demos/graphlan.bash
@@ -1,11 +1,21 @@
-# get the version information (demo developed with graphlan v0.9.7)
-graphlan.py --version
 
-# generate the tree structure and annotation file
-export2graphlan.py --skip_rows 1,2 -i $INPUT_FOLDER/merged_abundance_table.txt --tree $OUTPUT_FOLDER/merged_abundance.tree.txt --annotation $OUTPUT_FOLDER/merged_abundance.annot.txt --most_abundant 100 --abundance_threshold 1 --least_biomarkers 10 --annotations 5,6 --external_annotations 7 --min_clade_size 1
+graphlan.py $INPUT_FOLDER/guide.txt step_0.png --dpi 300 --size 3.5
+graphlan.py $INPUT_FOLDER/guide.txt step_0.svg --dpi 300 --size 3.5
 
-# run graphlan annotate on the demo input files
-graphlan_annotate.py --annot $OUTPUT_FOLDER/merged_abundance.annot.txt $OUTPUT_FOLDER/merged_abundance.tree.txt $OUTPUT_FOLDER/merged_abundance.xml
+graphlan_annotate.py --annot $INPUT_FOLDER/annot_0.txt $INPUT_FOLDER/guide.txt $OUTPUT_FOLDER/guide_1.xml
+graphlan.py $OUTPUT_FOLDER/guide_1.xml $OUTPUT_FOLDER/step_1.png --dpi 300 --size 3.5
+graphlan.py $OUTPUT_FOLDER/guide_1.xml $OUTPUT_FOLDER/step_1.svg --dpi 300 --size 3.5
 
-# run graphlan
-graphlan.py --dpi 300 $OUTPUT_FOLDER/merged_abundance.xml $OUTPUT_FOLDER/merged_abundance.png --external_legends
+graphlan_annotate.py --annot $INPUT_FOLDER/annot_1.txt $OUTPUT_FOLDER/guide_1.xml $OUTPUT_FOLDER/guide_2.xml
+graphlan.py $OUTPUT_FOLDER/guide_2.xml $OUTPUT_FOLDER/step_2.png --dpi 300 --size 3.5
+graphlan.py $OUTPUT_FOLDER/guide_2.xml $OUTPUT_FOLDER/step_2.svg --dpi 300 --size 3.5
+
+graphlan_annotate.py --annot $INPUT_FOLDER/annot_2.txt $OUTPUT_FOLDER/guide_2.xml $OUTPUT_FOLDER/guide_3.xml
+graphlan.py $OUTPUT_FOLDER/guide_3.xml $OUTPUT_FOLDER/step_3.png --dpi 300 --size 3.5
+graphlan.py $OUTPUT_FOLDER/guide_3.xml $OUTPUT_FOLDER/step_3.svg --dpi 300 --size 3.5
+
+graphlan_annotate.py --annot $INPUT_FOLDER/annot_3.txt $OUTPUT_FOLDER/guide_3.xml $OUTPUT_FOLDER/guide_4.xml
+graphlan.py $OUTPUT_FOLDER/guide_4.xml $OUTPUT_FOLDER/step_4.png --dpi 300 --size 3.5 --pad 0.0
+graphlan.py $OUTPUT_FOLDER/guide_4.xml $OUTPUT_FOLDER/step_4.svg --dpi 300 --size 3.5 --pad 0.0
+
+
