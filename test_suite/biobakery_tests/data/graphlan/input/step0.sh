688945948ccb859b83667f28f8e6d4ca0da4cc33
ljmciver <lauren.j.mciver@gmail.com>
2022-06-08 16:09:38 +0000

    Test suite: Add latest graphlan tutorial files

---
+++ test_suite/biobakery_tests/data/graphlan/input/step0.sh
@@ -0,0 +1,2 @@
+graphlan.py guide.txt step_0.png --dpi 300 --size 3.5
+graphlan.py guide.txt step_0.svg --dpi 300 --size 3.5
---
+++ test_suite/biobakery_tests/data/graphlan/input/step3.sh
@@ -0,0 +1,3 @@
+graphlan_annotate.py --annot annot_2.txt guide_2.xml	guide_3.xml
+graphlan.py guide_3.xml step_3.png --dpi 300 --size 3.5
+graphlan.py guide_3.xml step_3.svg --dpi 300 --size 3.5
