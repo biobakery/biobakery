faa658b65e03a8af3d6ea96890b9273237ae0369
ljmciver <lauren.j.mciver@gmail.com>
2023-08-14 19:42:56 +0000

    Update kneaddata demo to latest version

--- test_suite/biobakery_tests/demos/kneaddata.bash
+++ test_suite/biobakery_tests/demos/kneaddata.bash
@@ -5,13 +5,12 @@ kneaddata_read_count_table --input $OUTPUT_FOLDER/kneaddataOutputSingleEnd --out
 kneaddata --unpaired $INPUT_FOLDER/SE_extra.fastq --reference-db $INPUT_FOLDER/demo_db --output $OUTPUT_FOLDER/kneaddataOutputFastQC --run-fastqc-start --run-fastqc-end
     
 kneaddata --input1 $INPUT_FOLDER/seq1.fastq --input2 $INPUT_FOLDER/seq2.fastq --reference-db $INPUT_FOLDER/demo_db --output $OUTPUT_FOLDER/kneaddataOutputPairedEnd 
-    
-kneaddata --unpaired $INPUT_FOLDER/singleEnd.fastq --output $OUTPUT_FOLDER/kneaddataOutputTrimmomatic --reference-db $INPUT_FOLDER/demo_db --trimmomatic-options="MINLEN:90"
 
-kneaddata_read_count_table --input $OUTPUT_FOLDER/kneaddataOutputTrimmomatic/ --output $OUTPUT_FOLDER/kneaddataOutputTrimmomatic/kneaddata_read_count_table2.tsv
-
-kneaddata --unpaired $INPUT_FOLDER/singleEnd.fastq --output $OUTPUT_FOLDER/kneaddataOutputBowtie2 --reference-db $INPUT_FOLDER/demo_db --bowtie2-options="--very-fast" --bowtie2-options="-p 2"
+kneaddata --unpaired input/singleEnd.fastq --output $OUTPUT_FOLDER/kneaddataOutputTrimmomatic2 --reference-db input/demo_db --trimmomatic-options="ILLUMINACLIP:/TruSeq3-SE.fa:2:30:10 SLIDINGWINDOW:4:20 MINLEN:90"
+kneaddata_read_count_table --input $OUTPUT_FOLDER/kneaddataOutputTrimmomatic/ --output $OUTPUT_FOLDER/kneaddata_read_count_table3.tsv
 
 kneaddata --unpaired $INPUT_FOLDER/singleEnd.fastq --reference-db $INPUT_FOLDER/demo_db --output $OUTPUT_FOLDER/kneaddataOutput --quality-scores phred64
+
+kneaddata --unpaired $INPUT_FOLDER/singleEnd.fastq --reference-db $INPUT_FOLDER/demo_db --output $OUTPUT_FOLDER/kneaddataOutput --threads 4 --processes 4
 
 
