411b0329cfef678e9bc2a8f0165fd275ff019e42
ljmciver <lauren.j.mciver@gmail.com>
2023-08-14 19:53:06 +0000

    Fix kneaddata output folder location

--- test_suite/biobakery_tests/demos/kneaddata.bash
+++ test_suite/biobakery_tests/demos/kneaddata.bash
@@ -7,7 +7,7 @@ kneaddata --unpaired $INPUT_FOLDER/SE_extra.fastq --reference-db $INPUT_FOLDER/d
 kneaddata --input1 $INPUT_FOLDER/seq1.fastq --input2 $INPUT_FOLDER/seq2.fastq --reference-db $INPUT_FOLDER/demo_db --output $OUTPUT_FOLDER/kneaddataOutputPairedEnd 
 
 kneaddata --unpaired $INPUT_FOLDER/singleEnd.fastq --output $OUTPUT_FOLDER/kneaddataOutputTrimmomatic2 --reference-db $INPUT_FOLDER/demo_db --trimmomatic-options="ILLUMINACLIP:/TruSeq3-SE.fa:2:30:10 SLIDINGWINDOW:4:20 MINLEN:90"
-kneaddata_read_count_table --input $OUTPUT_FOLDER/kneaddataOutputTrimmomatic/ --output $OUTPUT_FOLDER/kneaddata_read_count_table3.tsv
+kneaddata_read_count_table --input $OUTPUT_FOLDER/kneaddataOutputTrimmomatic2/ --output $OUTPUT_FOLDER/kneaddata_read_count_table3.tsv
 
 kneaddata --unpaired $INPUT_FOLDER/singleEnd.fastq --reference-db $INPUT_FOLDER/demo_db --output $OUTPUT_FOLDER/kneaddataOutput --quality-scores phred64
 
